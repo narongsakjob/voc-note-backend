d7b9e7998d0929d579053b6d7351084b825095af
Narongsak <narongsak.chobsri@gmail.com>
2018-03-20 15:12:24 +0700

    Add page 404 not found

--- server.js
+++ server.js
@@ -12,12 +12,13 @@ mongoose.connect('mongodb://localhost/Vocdb');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(function(req, res) {
-  res.status(404).send({url: req.originalUrl + ' not found'})
-});
 
 var routes = require('./api/routes/voc_route'); //importing route
 routes(app); //register the route
+
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
 
 app.listen(port)
 
